dbf6b6178a53d7973ed72641334c6dabe013321b
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2015-05-09 13:21:29 +0200

    template changes

--- src/main/resources/scripts/template.groovy
+++ src/main/resources/scripts/template.groovy
@@ -13,8 +13,8 @@ upload(destFolder)
 
 def upload(destination,
            String folderType = "cmis:folder",
-           String documentType = "cmisbox:versionedDocument",
-           VersioningState versioningState = VersioningState.MAJOR) {
+           String documentType = "cmis:document",
+           VersioningState versioningState = VersioningState.NONE) {
 
     println "Uploading...\n"
     doUpload(destination, index + "", folderType, documentType, versioningState)
@@ -26,11 +26,11 @@ def doUpload(Folder parent, String folder, String folderType, String documentTyp
     for (int i = 0; i < 100; i++) {
         Folder newFolder = cmis.createFolder(threadFolder, "folder_" + i, folderType)
         for (int j = 0; j < 10; j++) {
-            Folder newFolder2 = cmis.createFolder(newFolder, "subfolder_" + i + "_" + j, folderType)
-            cmis.createRandomContentDocument(newFolder,"document_" + i+ "_" + j ,(1024 * 1024* 4).toLong(), documentType, versioningState)
+            Folder newFolder2 = cmis.createFolder(newFolder, "subfolder_"  + j, folderType)
+            cmis.createRandomContentDocument(newFolder,"document_"  + j ,(1024 * 1024* 1).toLong(), documentType, versioningState)
             Thread.sleep(0)
             for (int k = 0; k < 10; k++) {
-                cmis.createFolder(newFolder2, "subfolder_" + i + "_" + j + "_" + k, folderType)
+                cmis.createFolder(newFolder2, "subfolder_" + k, folderType)
             }
             Thread.sleep(0)
         }
